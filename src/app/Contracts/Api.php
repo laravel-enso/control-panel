758650ab531bee6de3120fe7e4da70d0f9ad5be9
Adrian Ocneanu <aocneanu@gmail.com>
2020-02-01 14:33:17 +0000

    Apply fixes from StyleCI

--- src/app/Contracts/Api.php
+++ src/app/Contracts/Api.php
@@ -2,8 +2,6 @@
 
 namespace LaravelEnso\ControlPanel\App\Contracts;
 
-use Psr\Http\Message\ResponseInterface;
-
 interface Api
 {
     public function response(string $method, string $uri): ?array;
