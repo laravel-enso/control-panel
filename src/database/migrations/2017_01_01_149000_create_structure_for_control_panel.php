7ac215baf5809c64f612a3cf9623a12a225d2cd7
mishu <mishu@xtelecom.ro>
2020-03-14 18:10:14 +0200

    removes Types from migrations

--- src/database/migrations/2017_01_01_149000_create_structure_for_control_panel.php
+++ src/database/migrations/2017_01_01_149000_create_structure_for_control_panel.php
@@ -1,16 +1,15 @@
 <?php
 
 use LaravelEnso\Migrator\App\Database\Migration;
-use LaravelEnso\Permissions\App\Enums\Types;
 
 class CreateStructureForControlPanel extends Migration
 {
     protected $permissions = [
-        ['name' => 'controlPanel.index', 'description' => 'General view', 'type' => Types::Read, 'is_default' => false],
-        ['name' => 'controlPanel.statistics', 'description' => 'Get metrics for an application', 'type' => Types::Read, 'is_default' => false],
-        ['name' => 'controlPanel.actions', 'description' => 'Get actions for an application', 'type' => Types::Read, 'is_default' => false],
-        ['name' => 'controlPanel.action', 'description' => 'Do action for an application', 'type' => Types::Write, 'is_default' => false],
-        ['name' => 'controlPanel.gitlab', 'description' => 'Get metrics for gitlab of an application', 'type' => Types::Read, 'is_default' => false],
-        ['name' => 'controlPanel.sentry', 'description' => 'Get metrics for sentry of an application', 'type' => Types::Read, 'is_default' => false],
+        ['name' => 'controlPanel.index', 'description' => 'General view', 'is_default' => false],
+        ['name' => 'controlPanel.statistics', 'description' => 'Get metrics for an application', 'is_default' => false],
+        ['name' => 'controlPanel.actions', 'description' => 'Get actions for an application', 'is_default' => false],
+        ['name' => 'controlPanel.action', 'description' => 'Do action for an application', 'is_default' => false],
+        ['name' => 'controlPanel.gitlab', 'description' => 'Get metrics for gitlab of an application', 'is_default' => false],
+        ['name' => 'controlPanel.sentry', 'description' => 'Get metrics for sentry of an application', 'is_default' => false],
     ];
 }
--- src/database/migrations/2017_01_01_149500_create_structure_for_applications.php
+++ src/database/migrations/2017_01_01_149500_create_structure_for_applications.php
@@ -1,20 +1,19 @@
 <?php
 
 use LaravelEnso\Migrator\App\Database\Migration;
-use LaravelEnso\Permissions\App\Enums\Types;
 
 class CreateStructureForApplications extends Migration
 {
     protected $permissions = [
-        ['name' => 'administration.applications.initTable', 'description' => 'Init table for applications', 'type' => Types::Read, 'is_default' => false],
-        ['name' => 'administration.applications.tableData', 'description' => 'Get table data for applications', 'type' => Types::Read, 'is_default' => false],
-        ['name' => 'administration.applications.exportExcel', 'description' => 'Export excel for applications', 'type' => Types::Read, 'is_default' => false],
-        ['name' => 'administration.applications.index', 'description' => 'Control Panel index', 'type' => Types::Read, 'is_default' => false],
-        ['name' => 'administration.applications.create', 'description' => 'Create application', 'type' => Types::Write, 'is_default' => false],
-        ['name' => 'administration.applications.destroy', 'description' => 'Delete application', 'type' => Types::Write, 'is_default' => false],
-        ['name' => 'administration.applications.edit', 'description' => 'Edit application', 'type' => Types::Read, 'is_default' => false],
-        ['name' => 'administration.applications.store', 'description' => 'Store application', 'type' => Types::Write, 'is_default' => false],
-        ['name' => 'administration.applications.update', 'description' => 'Update application', 'type' => Types::Read, 'is_default' => false],
+        ['name' => 'administration.applications.initTable', 'description' => 'Init table for applications', 'is_default' => false],
+        ['name' => 'administration.applications.tableData', 'description' => 'Get table data for applications', 'is_default' => false],
+        ['name' => 'administration.applications.exportExcel', 'description' => 'Export excel for applications', 'is_default' => false],
+        ['name' => 'administration.applications.index', 'description' => 'Control Panel index', 'is_default' => false],
+        ['name' => 'administration.applications.create', 'description' => 'Create application', 'is_default' => false],
+        ['name' => 'administration.applications.destroy', 'description' => 'Delete application', 'is_default' => false],
+        ['name' => 'administration.applications.edit', 'description' => 'Edit application', 'is_default' => false],
+        ['name' => 'administration.applications.store', 'description' => 'Store application', 'is_default' => false],
+        ['name' => 'administration.applications.update', 'description' => 'Update application', 'is_default' => false],
     ];
 
     protected $menu = [
